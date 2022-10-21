0d1012251fbc3c4c3ee454f22836c373bbb9eda0
PhiLong <16619@student.vgu.edu.vn>
2022-10-21 09:37:51 +0700

    bruh

--- sona-test-network/1_setupandChaincode.sh
+++ sona-test-network/1_setupandChaincode.sh
@@ -1,4 +1,4 @@
 # create channel
 ./network.sh up createChannel
 # deploy chaincode 
-./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript -ccl javascript
+./network.sh deployCC -ccn basic -ccp ../sample/chaincode-javascript -ccl javascript
