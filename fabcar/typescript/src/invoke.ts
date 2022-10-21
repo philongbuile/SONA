b46b8a95e2c2a8b8016a55c4f6c7bb4bc1b5f883
PhiLong <16619@student.vgu.edu.vn>
2022-10-22 01:44:39 +0700

    adding CreateCase and ReadeCase function in chaincode

---
+++ fabcar/typescript/src/invoke.ts
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Gateway, Wallets } from 'fabric-network';
+import * as path from 'path';
+import * as fs from 'fs';
+
+async function main() {
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', '..','test-network','organizations','peerOrganizations','org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.ts application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+        
+
+        // Get the contract from the network.
+        const caseContract = network.getContract('fabcar','CaseContract');
+        const patientContract = network.getContract('fabcar','AssetTransferContract')
+
+        // Submit the specified transaction.
+        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
+        // await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+
+        await patientContract.submitTransaction('InitLedger');
+        console.log(`Transaction has been submitted`);
+        // await caseContract.submitTransaction('InitCaseLedger');
+        await caseContract.submitTransaction('CreateCase','1','success','allergic','use medicine');
+        console.log(`Transaction has been submitted`);
+
+        await caseContract.submitTransaction('CreateCase','2','success','flu','hide from project and team meeting');
+        console.log(`Transaction has been submitted`);
+        await caseContract.submitTransaction('CreateCase','2','failed','handsome','no treatment yet');
+        console.log(`Transaction has been submitted`);
+
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
