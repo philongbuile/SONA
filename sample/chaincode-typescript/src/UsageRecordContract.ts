01a1e56fe2def4d51e7a3da524d1c4766e98a95c
Phi Long Bùi Lê <80462415+philongbuile@users.noreply.github.com>
2022-10-23 15:13:16 +0700

    modify CreateRecord()
    add uuidv4 automatically generate id for records

--- sample/chaincode-typescript/src/UsageRecordContract.ts
+++ sample/chaincode-typescript/src/UsageRecordContract.ts
@@ -2,9 +2,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 // Deterministic JSON.stringify()
+import { V4MAPPED } from 'dns';
 import {Context, Contract, Info, Returns, Transaction} from 'fabric-contract-api';
 import stringify from 'json-stringify-deterministic';
 import sortKeysRecursive from 'sort-keys-recursive';
+import { v4 as uuidv4 } from 'uuid';
 import {Patient} from './asset';
 import {Case} from './asset';
 import {UsageRecord} from './asset';
@@ -14,14 +16,15 @@ import {AssetTransferContract} from './assetTransfer'
 export class UsageRecordContract extends Contract {
 
 
+    // temp function for create Record when query Case in Medical Info
     // create an object record then push it in to the Records array corresponding to the patient
     @Transaction()
-    public async CreateRecord(ctx: Context, patient_username: string, case_id: string, medicalinfo_id: string ,record_id: string,operation: string, roles: string ,operator_name: string): Promise<void>{
+    public async CreateRecord(ctx: Context, patient_username: string, case_id: string, medicalinfo_id: string ,operation: string, roles: string ,operator_name: string): Promise<void>{
         const record ={
             docType: 'UsageRecord',
             Case_ID: case_id,
             MedicalInfo_ID: medicalinfo_id,
-            Record_ID: record_id,
+            Record_ID: uuidv4(),
             Operation: operation,
             Roles: roles,
             OperatorName: operator_name,
