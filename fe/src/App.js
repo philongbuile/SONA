dae2ab74051044a67a7a8232894ec6dce1f81858
minhleduc0210 <bastreet124@gmail.com>
2022-10-29 17:20:29 +0700

    Front-end: Search By Key Word page, however, the list would suggest appropriate response if the input is fully correct.

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,13 +1,17 @@
 import LoginPage from './pages/LoginPage';
 import UserProfile from './pages/userProfile';
 import Examination from './pages/Examination';
+import SearchByKeyWord from './pages/SearchByKeyWord'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/:username/:medicalInfoID" element={<Examination />} />
+          <Route path="/" element={<SearchByKeyWord />} />
       </Routes>
     </BrowserRouter>
   )
