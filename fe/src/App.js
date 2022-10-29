ca8813b30df8563c6d9dede9451d8bfa0b5e7bf0
minhleduc0210 <bastreet124@gmail.com>
2022-10-30 00:32:44 +0700

    Fetching (Sample)

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import LoginPage from './pages/LoginPage';
 import UserProfile from './pages/userProfile';
 import Examination from './pages/Examination';
+import SearchByKeyWord from './pages/SearchByKeyWord'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 
@@ -9,8 +10,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
           <Route path="/:username/:medicalInfoID" element={<Examination />} />
+          <Route path="/" element={<SearchByKeyWord />} />
       </Routes>
     </BrowserRouter>
   );
