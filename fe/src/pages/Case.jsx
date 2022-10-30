5393d3cf2a23318d008cfbfcfc264966a8fddf4a
minhleduc0210 <bastreet124@gmail.com>
2022-10-30 22:40:51 +0700

    Done: SearchByKeyWord and Case

--- fe/src/pages/Case.jsx
+++ fe/src/pages/Case.jsx
@@ -9,33 +9,33 @@ const Case = () => {
     const {id} = useParams();
     const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
     return(
-        <div className="Case">
+        <div className="box">
             <div>
 
             </div>
             {isPending && <div> Loading ... </div>}
             {error && <div> {error} </div>}
             {data && (
-                <div className='container"'> 
+                <div className='container'> 
                     <div className='container-title'>
                         <h2>Title</h2>
                     </div>
                     <div className='container-context'>
-                    <div>Operation Name:
+                    <div className='container-text-box'>Operation Name:
                         <div>
-                            <p className ='data-text'>{data.OperatorName}</p>
+                            <p className ='container-text'>{data.OperatorName}</p>
                         </div>
+                    </div >
+                    <div className='container-text-box'>Operation:
+                        <p className ='container-text'>{data.Operation}</p>
                     </div>
-                    <div>Operation:
-                        <p className ='data-text'>{data.Operation}</p>
+                    <div className='container-text-box'> Role:
+                        <p className ='container-text'>{data.Roles}</p>
                     </div>
-                    <div>Role:
-                        <p className ='data-text'>{data.Roles}</p>
+                    <div className='container-text-box'>Record ID:
+                        <p className ='container-text'>{data.Record_ID}</p>
                     </div>
-                    <div>Record ID:
-                        <p className ='data-text'>{data.Record_ID}</p>
-                    </div>
-                    <div>Time:
+                    <div className='container-text-box'>Time:
                         <p className ='data-text'>{data.Times}</p>
                     </div>
                     </div>
