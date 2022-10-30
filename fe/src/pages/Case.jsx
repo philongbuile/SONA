69da92967795dd68aa7337b88681b999007f7375
minhleduc0210 <bastreet124@gmail.com>
2022-10-30 20:28:14 +0700

    Struggle with fetching data with route parameters (the ID is not unique could be a reason for this), also, design for the Case

--- fe/src/pages/Case.jsx
+++ fe/src/pages/Case.jsx
@@ -1,38 +1,47 @@
 import Navbar from '../components/navbar'
 import { useParams } from 'react-router-dom'
 import useFetch from '../api/UseFetch'
+import './Case.css'
 
 
 const Case = () => {
 
     const {id} = useParams();
-    const {data: card, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
+    const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
     return(
-        <div className="controller-grid">
+        <div className="Case">
+            <div>
+
+            </div>
             {isPending && <div> Loading ... </div>}
             {error && <div> {error} </div>}
-            {card && (
-                <article className=''>
-                    <div>
-                        <h2>{card.title}</h2>
+            {data && (
+                <div className='container"'> 
+                    <div className='container-title'>
+                        <h2>Title</h2>
                     </div>
-                    <div >
-                        <div>Test Result: 
-                            <p >{card.testResult}</p>
-                        </div>
-                        
-                        <div>Diagnosis: 
-                            <p >{card.diagnosis}</p>
-                        </div>
-                        <div>Treatmen:
-                            <p >{card.treatment}</p>
+                    <div className='container-context'>
+                    <div>Operation Name:
+                        <div>
+                            <p className ='data-text'>{data.OperatorName}</p>
                         </div>
                     </div>
-                </article>
+                    <div>Operation:
+                        <p className ='data-text'>{data.Operation}</p>
+                    </div>
+                    <div>Role:
+                        <p className ='data-text'>{data.Roles}</p>
+                    </div>
+                    <div>Record ID:
+                        <p className ='data-text'>{data.Record_ID}</p>
+                    </div>
+                    <div>Time:
+                        <p className ='data-text'>{data.Times}</p>
+                    </div>
+                    </div>
+                </div>
             )}
         </div>
-    
-
     )
 }
 
