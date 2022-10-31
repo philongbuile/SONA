5a3bfb548bcf90b20cfe741fa328b5a651a2a46e
minhleduc0210 <bastreet124@gmail.com>
2022-10-31 16:37:18 +0700

    Done Case, fetching /medinfo/patient_query_medicalinfo/.., testing medical1 medical 2

--- fe/src/pages/Case.jsx
+++ fe/src/pages/Case.jsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 import { useParams } from 'react-router-dom'
 import useFetch from '../api/UseFetch'
 import './Case.css'
