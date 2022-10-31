927f6b00c2d47f169823906905a1d19f046595e4
tuhuynh99 <hctu.study@gmail.com>
2022-10-31 15:45:42 +0700

    fix querybykeywords page

--- fe/src/pages/SearchByKeyWord.jsx
+++ fe/src/pages/SearchByKeyWord.jsx
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
  
 const SearchByKeyWord = () => {
-    const [filter, setFilter] = useState('');
+    const [keywords, setKeywords] = useState('[]');
     const [cards, setCards] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8080/record/getall')
+        fetch('http://localhost:8080/medinfo/query_by_keyword/' + keywords,
+        )
             .then(res => {
                 return res.json()
             })
@@ -17,7 +18,7 @@ const SearchByKeyWord = () => {
             .catch(error => {
                 return error;
             });
-    }, []);
+    }, [keywords]);
 
     return(
         <div className=''>
@@ -25,36 +26,38 @@ const SearchByKeyWord = () => {
                 <div className='row justify-content-center'> 
                     <div className='col-12 mb-5'>
                         <div className='mb-3 col-4 mx-auto text-center'>
-                            <label className='form-lable h4'>Search:&nbsp;&nbsp;</label>
-                            <input type="text" className='from-control'
-                                onChange={(event) => {
-                                    setFilter(event.target.value);
-                                }}
-                            />
-                           
+                        <form class="form-inline">
+                                
+                                <div class="form-group mx-sm-3 mb-2">
+                                    <label class="sr-only">Query By Keywords</label>
+                                    <input class="form-control" id="search-box" placeholder="ex: cancer, diabete"/>
+                                </div>
+                               
+                                <button type='button' class="btn btn-primary mb-2" onClick={ () => {
+                                    console.log('button clicked');
+                                    let kw_str = document.getElementById("search-box").value;
+                                    kw_str = kw_str.trim();
+                                    kw_str = kw_str.replace(/ +/g, "");
+                                    let kw = kw_str.split(',');
+                                    setKeywords(JSON.stringify(kw));
+                                    console.log(kw);
+
+                                }}>Search</button>
+                        </form>
+                            
                         </div>
                     </div>
 
-                    {cards.response.filter((res) => {
-                        if(filter == ''){
-                            return res
-                        }else if(res.title.toLowerCase().includes(filter.toLowerCase())){
-                            return res
-                        }
-                    }).map((item, index) => {
+                    {cards.response.map((item, index) => {
                         return(
                             <div className='col-12 col-md-6 col-lg-3 mx-0 mb-4' >
                                 {<div className='card p-0 overflow-hidden h-100 shadow'>
                                     {/* <img src={item.img} alt="" className='card-img-top img-fluid'/> */}
-                                    <div className='card-body' key={item.MedicalInfo_ID}>
-                                        <Link to={`/record/query/${item.MedicalInfo_ID}`}>
-                                        <h5 className='card-title'>{item.MedicalInfo_ID}</h5>
+                                    <div className='card-body' key={item.ID}>
+                                        <Link to={`/record/query/${item.ID}`}>
+                                        <h5 className='card-title'>{item.ID}</h5>
                                             <div className='panel-body'>
-                                                <p className ='card-text'>{item.OperatorName}</p>
-                                                <p className ='card-text'>{item.Operation}</p>
-                                                <p className ='card-text'>{item.Roles}</p>
-                                                <p className ='card-text'>{item.Record_ID}</p>
-                                                <p className ='card-text'>{item.Times}</p>
+                                                {/* <p className ='card-text'>{item.docType}</p>                                     */}
                                             </div>
                                         </Link>
                                         <button type='button' className='btn btn-primary'> Ask for Authorization</button>
