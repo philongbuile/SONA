88c27157fae43a47de7f1df027026a730f736803
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-11-01 10:17:07 +0700

    blablah

--- fe2/src/App.tsx
+++ fe2/src/App.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect } from 'react';
-import TableItem from './components/TableList/TableItem';
 import './App.scss';
 import './configs/antd/customized.css';
 import 'react-toastify/dist/ReactToastify.css';
-import UserProfile from './pages/UserProfile'
-import UserTable from './components/UserTable';
-
 import { ToastContainer } from 'react-toastify';
-
 import AppRoute from './routes/routes';
-import TableList from './components/TableList/TableList';
-import AppNavbar from './components/Navbar';
-import AuthorizationList from './pages/AuthorizationList';
 
 
 
 function App() {
   return (
-    <>
-    <TableList/>
-      {/* <ToastContainer
+    <div className="App">
+      <ToastContainer
         position="top-center"
         autoClose={2500}
         hideProgressBar={false}
@@ -29,8 +19,8 @@ function App() {
         pauseOnFocusLoss
         draggable
       /> 
-       <AppRoute /> */}
-    </>
+      <AppRoute />
+      </div>
   );
 }
 
--- fe2/src/pages/Landingpage.tsx
+++ fe2/src/pages/Landingpage.tsx
@@ -1,13 +1,17 @@
 import styles from '../assets/css/Landingpage.module.css';
 import Pill from '../components/Pill';
-
+import { useNavigate } from 'react-router-dom';
 
 const Landingpage = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/login');
+    }
     return (
         <div className={styles.Landingpage}>
             <Pill/>
             <h3>Your health is your privacy</h3>
-            <button className={styles.start_button} onClick={() => {console.log("clicked")}}>Get Started</button>
+            <button className={styles.start_button} onClick={() => {handleClick();}}>Get Started</button>
         </div>
     )
 }
