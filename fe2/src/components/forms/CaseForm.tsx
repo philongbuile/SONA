de45903269ab774f67275f8eea30bc208841a977
Trung gà <thomastom12091@gmail.com>
2022-11-01 15:11:18 +0700

    increase input size

--- fe2/src/components/forms/CaseForm.tsx
+++ fe2/src/components/forms/CaseForm.tsx
@@ -1,19 +1,21 @@
 import styles from '../../assets/css/CaseForm.module.css';
 import { Form, Input } from 'antd'
 
+const {TextArea} = Input;
+
 const CaseForm = () => {
     return ( 
         <Form className={styles.case_form}>
             <Form.Item>
-                <Input placeholder="test result"/>
+                <TextArea rows={5} placeholder="test result"/>
             </Form.Item>
 
             <Form.Item>
-                <Input placeholder="diagnosis"/>
+                <TextArea rows={5} placeholder="diagnosis"/>
             </Form.Item>
 
             <Form.Item>
-                <Input placeholder="treatment"/>
+                <TextArea rows={5} placeholder="treatment"/>
             </Form.Item>
         </Form>
     )
