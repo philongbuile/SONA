c26a80ebc0e13fd47ac564ab73addb55d73e13d5
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-11-01 22:09:26 +0700

    resolve

--- fe/src/components/profile.jsx
+++ fe/src/components/profile.jsx
@@ -1,10 +1,6 @@
 import { Card } from 'antd';
 import './Profile.css';
-<<<<<<< HEAD
 // import avatar from '../assets/avatar.png';
-=======
-import avatar from '../assets/avatar.png';
->>>>>>> origin
 import { PersonalInforFetch } from '../api/userApi';
 import { useParams } from 'react-router-dom';
 
