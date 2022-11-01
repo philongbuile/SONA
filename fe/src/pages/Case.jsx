3d4494e81c41b95137c0f953adfd7be5582228d2
minhleduc0210 <bastreet124@gmail.com>
2022-11-01 20:14:51 +0700

    Nothing has changed

--- fe/src/pages/Case.jsx
+++ fe/src/pages/Case.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import Navbar from '../components/Navbar'
-=======
 import Navbar from '../components/navbar'
->>>>>>> origin
 import { useParams } from 'react-router-dom'
 import useFetch from '../api/useFetch'
 import './Case.css'
@@ -11,22 +7,6 @@ import { PersonalInforFetch } from '../api/userApi';
 const Case = () => {
 
     const {id} = useParams();
-<<<<<<< HEAD
-    // const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
-    const { data, error, isPending } = PersonalInforFetch('philong123');
-
-    console.log(data);
-    // console.log(personalData);
-    return(
-        <div className="box">
-            {isPending && <div className='loader'> </div>}
-            {error && <div> {error} </div>}
-            {data && (
-                <div className='container'> 
-                    <div className='container-title'>
-                        <h2>Title</h2>
-                    </div>
-=======
     const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
     const { data: personalData, isPendingP, errorP } = PersonalInforFetch('philong123');
 
@@ -43,7 +23,6 @@ const Case = () => {
                         <h2>CASE ID: {case_.response.ID}</h2>
                     </div>
 
->>>>>>> origin
                     <div className='container-context'>
                     <div className='container-text-box'>Operation Name:
                         <div>
@@ -59,35 +38,22 @@ const Case = () => {
                     <div className='container-text-box'>Record ID:
                         <p className ='container-text'>{data.response[0].Record_ID}</p>
                     </div>
-<<<<<<< HEAD
                     {/* <div className='container-text-box'>Time:
                         <p className ='data-text'>{data.response[0].Time}</p>
 
-=======
-                    <div className='container-text-box'>Time:
-                        <p className ='data-text'>{data.response[0].Time}</p>
 
 
 
-
->>>>>>> origin
                         <p className ='data-text'>{personalData.response.Username}</p>
 
                         <p className ='data-text'>{personalData.response.FullName}</p>
 
                         <p className ='data-text'>{personalData.response.Medicalinfo_ID}</p>
 
-<<<<<<< HEAD
                     </div> */}
                     </div>
                 </div>
-            )}
-=======
-                    </div>
-                    </div>
-                </div>
             }
->>>>>>> origin
         </div>
     )
 }
