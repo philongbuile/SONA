b2b986904379098157b1d3690b258ca6073c0237
minhleduc0210 <bastreet124@gmail.com>
2022-11-01 20:14:51 +0700

    Nothing has changed

--- fe/src/pages/Case.jsx
+++ fe/src/pages/Case.jsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import Navbar from '../components/Navbar'
+=======
+import Navbar from '../components/navbar'
+>>>>>>> 3d4494e8 (Nothing has changed)
 import { useParams } from 'react-router-dom'
 import useFetch from '../api/useFetch'
 import './Case.css'
@@ -7,20 +11,22 @@ import { PersonalInforFetch } from '../api/userApi';
 const Case = () => {
 
     const {id} = useParams();
-    // const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
-    const { data, error, isPending } = PersonalInforFetch('philong123');
+    const {data, error, isPending} = useFetch('http://localhost:8080/record/query/' + id);
+    const { data: personalData, isPendingP, errorP } = PersonalInforFetch('philong123');
 
     console.log(data);
-    // console.log(personalData);
+    console.log(personalData);
     return(
         <div className="box">
+            <Navbar/>
             {isPending && <div className='loader'> </div>}
             {error && <div> {error} </div>}
-            {data && (
+            {case_ && 
                 <div className='container'> 
                     <div className='container-title'>
-                        <h2>Title</h2>
+                        <h2>MEDICAL ID: {case_.response.ID}</h2>
                     </div>
+
                     <div className='container-context'>
                     <div className='container-text-box'>Operation Name:
                         <div>
@@ -39,6 +45,9 @@ const Case = () => {
                     {/* <div className='container-text-box'>Time:
                         <p className ='data-text'>{data.response[0].Time}</p>
 
+
+
+
                         <p className ='data-text'>{personalData.response.Username}</p>
 
                         <p className ='data-text'>{personalData.response.FullName}</p>
@@ -48,7 +57,7 @@ const Case = () => {
                     </div> */}
                     </div>
                 </div>
-            )}
+            }
         </div>
     )
 }
