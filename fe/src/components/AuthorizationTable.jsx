65f1776c55896980cb81be67c566e21a6c0e72e5
tuhuynh99 <hctu.study@gmail.com>
2022-11-07 17:15:56 +0700

    refactor

--- fe/src/components/AuthorizationTable.jsx
+++
@@ -1,9 +0,0 @@
-import {Table} from 'antd';
-
-const AuthorizationTable = (props) => {   
-    return (
-        <Table className = 'authorizationTable' columns={props.column} dataSource={props.row} />
-    );
-}
-
-export default AuthorizationTable;
