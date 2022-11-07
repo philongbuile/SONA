17248e2f8242a2c086aafb3c483fe918ace09541
tuhuynh99 <hctu.study@gmail.com>
2022-11-07 17:35:51 +0700

    refactor

--- fe/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
